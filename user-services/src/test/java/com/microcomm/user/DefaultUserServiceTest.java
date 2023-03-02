fb5ec4c64d7dd4b25faeffaa52ec4ab628612a10
Imad Eddine BOUIHI <iebouihi@sqli.com>
2023-03-03 00:48:50 +0100

    Unit Test

--- user-services/src/test/java/com/microcomm/user/DefaultUserServiceTest.java
+++ user-services/src/test/java/com/microcomm/user/DefaultUserServiceTest.java
@@ -1,5 +1,33 @@
 package com.microcomm.user;
 
 
+import com.microcomm.user.domaine.User;
+import com.microcomm.user.repository.UserRepository;
+import com.microcomm.user.service.UserService;
+import com.microcomm.user.service.impl.DefaultUserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class DefaultUserServiceTest {
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUpTest() {
+        userRepository = mock(UserRepository.class);
+
+    }
+
+    @Test
+    void testRegisterNewUser() {
+        UserService userService = new DefaultUserService();
+        User user1 = new User();
+        user1.setEmail("user1@email.com");
+        when(userRepository.findUserByEmail("user1@email.com")).thenReturn(user1);
+        assert (user1.getEmail() == "user1@email.com");
+
+    }
+
 }
